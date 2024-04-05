0c2befb36817b7d1c79b1d6c88de778f8b303ad5
BlcMed <boulaich.mohamed970@gmail.com>
2024-04-05 18:32:05 +0000

    outline

---
+++ src/extract_features.py
@@ -0,0 +1,9 @@
+
+def extract_LPC():
+    return 0
+
+def extract_PLP():
+    return 0
+
+def exract_MFCC():
+    return 0
---
+++ src/parse_audio.py
@@ -0,0 +1,6 @@
+
+def convet_to_spectrogram():
+    return 0
+
+def convert_to_mel_spectrogram():
+    return 0
