e8793805d4a6059a836fc3f1f7a631bfbcb7efb0
BlcMed <boulaich.mohamed970@gmail.com>
2024-05-08 16:07:45 +0100

    Update train pipline

--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -1,5 +1,5 @@
 from data_manager import load_audio_files, filter_data_based_on_accents
-from pipeline import pipeline
+from pipeline import make_pipeline
 from load_config import load_constants_from_yaml
 
 constants = load_constants_from_yaml('constants.yml')
@@ -17,7 +17,15 @@ CONSIDERED_ACCENTS = constants["CONSIDERED_ACCENTS"]
 
 def run_training():
     df = load_audio_files("data/raw/recordings/", sr = SAMPLING_RATING)
-    df = filter_data_based_on_accents(df=df, considered_accents=["english", "arabic"])
+    df = filter_data_based_on_accents(df=df, considered_accents=CONSIDERED_ACCENTS)
+    pipeline=make_pipeline(sampling_rating=SAMPLING_RATING,
+                           min_silence_duration=MIN_SILENCE_DURATION,
+                           threshold_percentage=THRESHOLD_PERCENTAGE,
+                           hop_length=HOP_LENGTH,
+                           frame_length_energy=FRAME_LENGTH_ENERGY,
+                           segment_duration=SEGMENT_DURATION,
+                           
+                           )
     df_transformed = pipeline.fit_transform(df)
     df_transformed.to_csv('df_transformed.csv')
 
